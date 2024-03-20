5bc57088385c08c0d5233779911993967365cf57
KumarAjit <ajit.sipu@gmail.com>
2024-03-20 22:43:14 +0530

    This is my first commit

---
+++ src/main/java/com/aspl/atms/service/ASPLATMSAppService/AsplAtmsAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aspl.atms.service.ASPLATMSAppService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsplAtmsAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsplAtmsAppServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aspl/atms/service/ASPLATMSAppService/AsplAtmsAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspl.atms.service.ASPLATMSAppService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsplAtmsAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
